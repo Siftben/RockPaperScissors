da58a6fe80879202979641c1c47ed703273befcd
Siftben <yobel.oktavinus98@gmail.com>
2023-08-12 17:57:18 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,84 @@
+let playerWin = 0, computerWin = 0;
+
+let playerSelection = prompt("Paper, Rock or Scissors?");
+playerSelection = playerSelection.toLowerCase();
+playerChoice(playerSelection);
+
+const computerSelection = getComputerChoice();
+alert("Computer choose " + computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
+
+for (let i = 1; i <= 5; i++) {
+    let playerSelection = prompt("Paper, Rock or Scissors?");
+    playerSelection = playerSelection.toLowerCase();
+    playerChoice(playerSelection);
+
+    const computerSelection = getComputerChoice();
+    alert("Computer choose " + computerSelection);
+
+    console.log(playRound(playerSelection, computerSelection));
+}
+
+alert("You win " + playerWin + " ,Computer win " + computerWin);
+
+//console.log(playRound(playerSelection, computerSelection));
+
+function playerChoice(playerSelection) {
+    if(playerSelection === "rock"){
+        alert("You choose rock!");
+    }
+    else if(playerSelection === "paper"){
+        alert("You choose paper!");
+    }
+    else if(playerSelection === "scissors"){
+        alert("You choose scissors!");
+    }
+    else{
+        alert("You choose nothing!");
+    }
+}
+
+function getComputerChoice() {
+    let x = Math.floor(Math.random() * 3) + 1;
+    
+    switch (x) {
+        case 1:
+            return("rock");
+            break;
+        case 2:
+            return("paper");
+            break;
+        case 3:
+            return("scissors");
+            break;
+    
+        default:
+            return("error");
+            break;
+    }
+    
+}
+
+function playRound(playerSelection, computerSelection) {
+  if(playerSelection === computerSelection){
+    alert("It's a draw");
+  }
+
+  else if (playerSelection === "paper" && computerSelection === "rock" || playerSelection === "rock" && computerSelection === "scissors" 
+  || playerSelection === "scissors" && computerSelection === "paper") {
+    alert("You Win!");
+    playerWin = playerWin + 1;
+  }
+  
+  else if (computerSelection === "paper" && playerSelection === "rock" || computerSelection === "rock" && playerSelection === "scissors" 
+  || computerSelection === "scissors" && playerSelection === "paper") {
+    alert("You Lose!");
+    computerWin = computerWin + 1;
+  }
+
+}
+
+function game(params) {
+    
+}
