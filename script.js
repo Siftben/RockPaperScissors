27569326206bb72080f6c6b357c8c6517a9f4bf2
Siftben <yobel.oktavinus98@gmail.com>
2023-08-12 18:05:34 +0900

    Cleaning the code

--- script.js
+++ script.js
@@ -65,13 +65,15 @@ function playRound(playerSelection, computerSelection) {
     alert("It's a draw");
   }
 
-  else if (playerSelection === "paper" && computerSelection === "rock" || playerSelection === "rock" && computerSelection === "scissors" 
+  else if (playerSelection === "paper" && computerSelection === "rock" 
+  || playerSelection === "rock" && computerSelection === "scissors" 
   || playerSelection === "scissors" && computerSelection === "paper") {
     alert("You Win!");
     playerWin = playerWin + 1;
   }
   
-  else if (computerSelection === "paper" && playerSelection === "rock" || computerSelection === "rock" && playerSelection === "scissors" 
+  else if (computerSelection === "paper" && playerSelection === "rock" 
+  || computerSelection === "rock" && playerSelection === "scissors" 
   || computerSelection === "scissors" && playerSelection === "paper") {
     alert("You Lose!");
     computerWin = computerWin + 1;
